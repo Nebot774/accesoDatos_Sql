6e721cc1ea60219b232feba52fa126447b20750f
Nebot774 <adriclemenbot@gmail.com>
2023-11-09 19:38:57 +0100

    avançe considerable

--- src/main/java/org/example/OperacionesCRUDPilotos.java
+++ src/main/java/org/example/OperacionesCRUDPilotos.java
@@ -1,8 +1,118 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OperacionesCRUDPilotos {
+    private Connection connection;
 
+    public OperacionesCRUDPilotos(Connection connection) {
+        this.connection = connection;
+    }
 
+    public void CrearPiloto(Piloto piloto) throws SQLException {
+        String sql = "INSERT INTO drivers (code, forename, surname, dob, nationality, url) VALUES (?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, piloto.getCode());
+            pstmt.setString(2, piloto.getForename());
+            pstmt.setString(3, piloto.getSurname());
+            pstmt.setString(4, piloto.getDob());
+            pstmt.setString(5, piloto.getNationality());
+            pstmt.setString(6, piloto.getUrl());
+            pstmt.executeUpdate();
+        }
+    }
 
+    public Piloto LeerPiloto(int driverId) throws SQLException {
+        String sql = "SELECT * FROM drivers WHERE driverid = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, driverId);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return new Piloto(
+                        rs.getInt("driverid"),
+                        rs.getString("code"),
+                        rs.getString("forename"),
+                        rs.getString("surname"),
+                        rs.getString("dob"),
+                        rs.getString("nationality"),
+                        rs.getString("url")
+                );
+            }
+        }
+        return null;
+    }
 
+    public List<Piloto> LeerPilotos() throws SQLException {
+        List<Piloto> pilotos = new ArrayList<>();
+        String sql = "SELECT * FROM drivers";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                pilotos.add(new Piloto(
+                        rs.getInt("driverid"),
+                        rs.getString("code"),
+                        rs.getString("forename"),
+                        rs.getString("surname"),
+                        rs.getString("dob"),
+                        rs.getString("nationality"),
+                        rs.getString("url")
+                ));
+            }
+        }
+        return pilotos;
+    }
+
+    public void ActualizarPiloto(Piloto piloto) throws SQLException {
+        String sql = "UPDATE drivers SET code = ?, forename = ?, surname = ?, dob = ?, nationality = ?, url = ? WHERE driverid = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, piloto.getCode());
+            pstmt.setString(2, piloto.getForename());
+            pstmt.setString(3, piloto.getSurname());
+            pstmt.setString(4, piloto.getDob());
+            pstmt.setString(5, piloto.getNationality());
+            pstmt.setString(6, piloto.getUrl());
+            pstmt.setInt(7, piloto.getDriverId());
+            pstmt.executeUpdate();
+        }
+    }
+
+    public void BorrarPiloto(Piloto piloto) throws SQLException {
+        String sql = "DELETE FROM drivers WHERE driverid = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, piloto.getDriverId());
+            pstmt.executeUpdate();
+        }
+    }
+
+    public void MostrarClasificacionPiloto() throws SQLException {
+        String sql = "SELECT d.forename, d.surname, SUM(r.points) as points FROM drivers d " +
+                "JOIN results r ON d.driverid = r.driverid " +
+                "GROUP BY d.driverid " +
+                "ORDER BY points DESC";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("forename") + " " + rs.getString("surname") + " - " + rs.getInt("points"));
+            }
+        }
+    }
+
+    public void MostrarClasificacionConstructores() throws SQLException {
+        String sql = "SELECT c.name, SUM(r.points) as points FROM constructors c " +
+                "JOIN results r ON c.constructorid = r.constructorid " +
+                "GROUP BY c.constructorid " +
+                "ORDER BY points DESC";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("name") + " - " + rs.getInt("points"));
+            }
+        }
+    }
 }
+
--- src/main/java/org/example/Piloto.java
+++ src/main/java/org/example/Piloto.java
@@ -1,12 +1,115 @@
 package org.example;
 
 
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Piloto {
+    private Integer driverId;
+    private String code;
+    private String forename;
+    private String surname;
+    private String dob; // Date of Birth in String format, ideally should be a Date type
+    private String nationality;
+    private String url;
 
+    // Constructor sin el ID del piloto, para cuando se cree uno nuevo y aún no se tenga un ID.
+    public Piloto(String code, String forename, String surname, String dob, String nationality, String url) {
+        this.code = code;
+        this.forename = forename;
+        this.surname = surname;
+        this.dob = dob;
+        this.nationality = nationality;
+        this.url = url;
+    }
 
+    // Constructor completo, incluyendo el ID del piloto, para cuando se recupere de la BD.
+    public Piloto(int driverId, String code, String forename, String surname, String dob, String nationality, String url) {
+        this(code, forename, surname, dob, nationality, url);
+        this.driverId = driverId;
+    }
 
+    // Getters y setters
+    public Integer getDriverId() {
+        return driverId;
+    }
 
+    public void setDriverId(Integer driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // Método toString() para imprimir la información del piloto.
+    @Override
+    public String toString() {
+        return "Piloto{" +
+                "driverId=" + driverId +
+                ", code='" + code + '\'' +
+                ", forename='" + forename + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dob='" + dob + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
 }
+
+
+
+
+
+
+
 
 
 
