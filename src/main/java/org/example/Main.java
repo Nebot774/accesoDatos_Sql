09684df3d9ac3d962a2045b418e7bd06a277493e
Nebot774 <adriclemenbot@gmail.com>
2023-11-14 17:44:38 +0100

    fin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,6 +16,33 @@ public class Main {
         try (Connection connection = DriverManager.getConnection(url)) {
             System.out.println("Conexión establecida con la base de datos.");
 
+            //hazemos una instancia de OperacionesCRUDPilotos
+            OperacionesCRUDPilotos operaciones = new OperacionesCRUDPilotos(connection);
+
+            //creamos un piloto
+            Piloto nuevoPiloto = new Piloto("56", "Serlli ", "peña", "2004-01-01", "Español", "http://url.com");
+            operaciones.CrearPiloto(nuevoPiloto);
+
+            // Leer un piloto por enteros
+            Piloto pilotoLeido = operaciones.LeerPiloto(56);
+            if (pilotoLeido != null) {
+                System.out.println("Piloto leído: " + pilotoLeido.getForename() + " " + pilotoLeido.getSurname());
+            }
+
+            // Actualizar un piloto
+            nuevoPiloto.setSurname("Blazquez");
+            operaciones.ActualizarPiloto(nuevoPiloto);
+
+            // Borrar un piloto
+            operaciones.BorrarPiloto(nuevoPiloto);
+
+            // Mostrar clasificación de pilotos
+            operaciones.MostrarClasificacionPiloto();
+
+            // Mostrar clasificación de constructores
+            operaciones.MostrarClasificacionConstructores();
+
+
         } catch (SQLException e) {
             System.out.println("Error al conectarse a la base de datos: " + e.getMessage());
         }
